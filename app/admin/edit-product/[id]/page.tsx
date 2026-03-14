80d3b5c415ed3ef4c20a1ade956b4c0d14775d84
rishikeshjha1710 <goldenjha2016@gmail.com>
2026-03-14 21:08:37 +0530

    first project

--- app/admin/edit-product/[id]/page.tsx
+++ app/admin/edit-product/[id]/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
+
 import { useState, useEffect } from "react";
 import { useRouter, useParams } from "next/navigation";
 import { getProductById, updateProduct } from "@/lib/firestore";
